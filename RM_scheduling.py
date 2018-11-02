6b67d60e0134acbe80f88e469ce3eb175cc8002a
ragesh <sonuragesh18@gmail.com>
2018-11-03 00:43:54 +0100

    RM_scheduling working properly

--- RM_scheduling.py
+++ RM_scheduling.py
@@ -10,6 +10,7 @@ from math import gcd
 from collections import OrderedDict
 
 tasks = dict()
+RealTime_task = dict() 
 T = []
 C = []
 U = []
@@ -20,6 +21,8 @@ def Read_data():
 	# Reading number of tasks to be scheduled
 	global n
 	global hp
+	global tasks
+
 	n = int(input("\n \t\tEnter number of Tasks:"))
 
 	#  Storing data in a dictionary
@@ -33,7 +36,7 @@ def Read_data():
 		tasks[i]["WCET"] = int(w)
 
 	# Writing the dictionary into a JSON file
-	with open('/home/ragesh/SCHEDULING/tasks.json','w') as outfile:
+	with open('/home/ragesh/SCHEDULING/RM_scheduling/tasks.json','w') as outfile:
 		json.dump(tasks,outfile,indent = 4)
 
 
@@ -72,8 +75,6 @@ def Schedulablity():
 		return False
 
 
-def Exact_schedulablity():
-	# For the time being this is not required	
 	return False
 def estimatePriority(RealTime_task):
 	lessPeriod = hp	
@@ -81,42 +82,40 @@ def estimatePriority(RealTime_task):
 	for i in RealTime_task.keys():
 		if (RealTime_task[i]["WCET"] != 0):
 			if (lessPeriod > RealTime_task[i]["Period"]):
-				P = i
 				lessPeriod = RealTime_task[i]["Period"]
+				P = i
 	return P
 
 
 def Simulation(hp):
 	# Real time scheduling are carried out in RealTime_task
-	RealTime_task = dict(tasks)		
-	print(RealTime_task)
-	
-	
+	RealTime_task = copy.deepcopy(tasks)	
+	print("before simulation", RealTime_task)
 	for t in range(hp):
 
 		# validation of schedulablity neessary condition	
 		for i in RealTime_task.keys():
 			if (RealTime_task[i]["WCET"] > RealTime_task[i]["Period"]):
-				print(" \n\t The task can not be cmopleted in the specified time ! ", i )
+				print(" \n\t The task can not be completed in the specified time ! ", i )
 
 		# Determine the priority of the given tasks
 		priority = estimatePriority(RealTime_task)
+
 		if (priority != -1):
 			print("\nt{}-->t{} :TASK{}".format(t,t+1,priority))
+			# Update WCET after each clock cycle 
+			RealTime_task[priority]["WCET"] -= 1
+
 		else:
 			print("\nt{}-->t{} :IDLE".format(t,t+1))			
 
-		# Update WCET time after execution 
-		RealTime_task[priority]["WCET"] -= 1
-
-		# Update period time
+		# Update Period after each clock cycle
 		for i in RealTime_task.keys():
 			RealTime_task[i]["Period"] -= 1
 			if (RealTime_task[i]["Period"] == 0):
-				RealTime_task = dict(tasks)
+				RealTime_task[i] = copy.deepcopy(tasks[i])
 
-
-
+			
 if __name__ == '__main__':
 	
 	Read_data()
@@ -126,4 +125,3 @@ if __name__ == '__main__':
 		Simulation(hp)
 	else:
 		Simulation(hp)
-		# Exact_schedulablity()
