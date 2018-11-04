1664fb9aa09288b18df62d71ecd610d8ea76d4fd
Ragesh Ramachandran <33636773+EnigmaRagesh@users.noreply.github.com>
2018-11-04 10:27:20 +0530

    added gann chart option

--- RM_scheduling.py
+++ RM_scheduling.py
@@ -5,40 +5,42 @@
 
 import json
 import copy
-import plotly.plotly as py
-import plotly.figure_factory as ff
 from sys import *
 from math import gcd
 from collections import OrderedDict
+import matplotlib.pyplot as plt
 
 tasks = dict()
-RealTime_task = dict() 
-d = dict()
-dList = []
+RealTime_task = dict()
 T = []
 C = []
 U = []
 
+x_axis  = []
+from_y = []
+to_y = []
 
 def Read_data():
+
 	# Reading number of tasks to be scheduled
 	global n
 	global hp
 	global tasks
 
-	n = int(input("\n\t\tEnter number of Tasks:"))
+	n = int(input("\n \t\tEnter number of Tasks:"))
+
 	#  Storing data in a dictionary
 	for i in range(n):
 		tasks[i] = {}
-		print("\nEnter Period of task T",i,":")
+		print("\n\n\n Enter Period of task T",i,":")
 		p = input()
 		tasks[i]["Period"] = int(p)
-		print("\nEnter the WCET of task C",i,":") 
-		w = input() 
+		print("Enter the WCET of task C",i,":")
+		w = input()
 		tasks[i]["WCET"] = int(w)
 
 	# Writing the dictionary into a JSON file
-	with open('/home/ragesh/SCHEDULING/RM_scheduling/tasks.json','w') as outfile:
+	with open('tasks.json','w') as outfile:
 		json.dump(tasks,outfile,indent = 4)
 
 
@@ -46,7 +48,7 @@ def Hyperperiod():
 
 	# Calculation of Hyper period
 	temp = []
-	for i in range(n):	
+	for i in range(n):
 		temp.append(tasks[i]["Period"])
 	HP = temp[0]
 	for i in temp[1:]:
@@ -79,7 +81,7 @@ def Schedulablity():
 
 	return False
 def estimatePriority(RealTime_task):
-	lessPeriod = hp	
+	lessPeriod = hp
 	P = -1
 	for i in RealTime_task.keys():
 		if (RealTime_task[i]["WCET"] != 0):
@@ -91,53 +93,46 @@ def estimatePriority(RealTime_task):
 
 def Simulation(hp):
 	# Real time scheduling are carried out in RealTime_task
-	RealTime_task = copy.deepcopy(tasks)	
+	RealTime_task = copy.deepcopy(tasks)
 	print("before simulation", RealTime_task)
 	for t in range(hp):
 
-		# validation of schedulablity neessary condition	
+		# validation of schedulablity neessary condition
 		for i in RealTime_task.keys():
 			if (RealTime_task[i]["WCET"] > RealTime_task[i]["Period"]):
 				print(" \n\t The task can not be completed in the specified time ! ", i )
 
 		# Determine the priority of the given tasks
 		priority = estimatePriority(RealTime_task)
-		global dList
 
 		if (priority != -1):
-			# Update WCET after each clock cycle 
-			RealTime_task[priority]["WCET"] -= 1
 			print("\nt{}-->t{} :TASK{}".format(t,t+1,priority))
-			d = dict(Task=priority, Start=t, Finish=t+1)      	
-			dList.append(d.copy())  							#dList is a list of dictionary d
-																#just used for plotting the output in a gantt chart
+			# Update WCET after each clock cycle
+			RealTime_task[priority]["WCET"] -= 1
+			x_axis.append("TASK%d"%priority)
+			from_y.append(t)
+			to_y.append(t+1)
+
 		else:
-			print("\nt{}-->t{} :IDLE".format(t,t+1))			
-			d = dict(Task="IDLE", Start=t, Finish=t+1)
-			dList.append(d.copy())
+			print("\nt{}-->t{} :IDLE".format(t,t+1))
+			x_axis.append("IDLE")
+			from_y.append(t)
+			to_y.append(t+1)
 
 		# Update Period after each clock cycle
 		for i in RealTime_task.keys():
 			RealTime_task[i]["Period"] -= 1
 			if (RealTime_task[i]["Period"] == 0):
-	
 				RealTime_task[i] = copy.deepcopy(tasks[i])
-	
-	# To plot the dList as a Gantt chart 
-	drawGantt()
 
 
-
-def drawGantt():
-	fig = ff.create_gantt(dList)
-	py.iplot(fig, filename='Rate Monotonic Scheduler', world_readable=True)
-			
 if __name__ == '__main__':
-	
+
 	Read_data()
 	sched_res = Schedulablity()
 	hp = Hyperperiod()
-	if sched_res == True:
-		Simulation(hp)
-	else:
-		Simulation(hp)
+	Simulation(hp)
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax = plt.hlines(x_axis, from_y, to_y)
+	plt.show()
