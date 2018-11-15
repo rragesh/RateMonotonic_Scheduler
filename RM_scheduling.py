d24ca7537ad6e1ecf65bc887eff87603e657d338
Ragesh Ramachandran <33636773+EnigmaRagesh@users.noreply.github.com>
2018-11-15 20:57:41 +0530

    Update RM_scheduling.py

--- RM_scheduling.py
+++ RM_scheduling.py
@@ -26,17 +26,25 @@ to_x = []
 
 def Read_data():
 	"""
-	Reading the details of the tasks to be scheduled from the user as 
+	Reading the details of the tasks to be scheduled from the user as
 	Number of tasks n:
 	Period of task P:
 	Worst case excecution time WCET:
-	""" 
+	"""
 	global n
 	global hp
 	global tasks
+	global dList
+
+	dList = {}
 
 	n = int(input("\n \t\tEnter number of Tasks:"))
 	#  Storing data in a dictionary
+	for  i in range(n):
+		dList["TASK_%d"%i] = {"start":[],"finish":[]}
+
+	dList["TASK_IDLE"] = {"start":[],"finish":[]}
+
 	for i in range(n):
 		tasks[i] = {}
 		print("\n\n\n Enter Period of task T",i,":")
@@ -65,8 +73,8 @@ def Hyperperiod():
 
 def Schedulablity():
 	"""
-	Calculates the utilization factor of the tasks to be scheduled 
-	and then checks for the schedulablity and then returns true is 
+	Calculates the utilization factor of the tasks to be scheduled
+	and then checks for the schedulablity and then returns true is
 	schedulable else false.
 	"""
 	for i in range(n):
@@ -96,7 +104,7 @@ def Schedulablity():
 			print("\n\tTasks are not schedulable by Rate Monotonic Scheduling!")
 			return False
 	print("\n\tOverloaded tasks!")
-	print("\n\tUtilization factor > 1")		
+	print("\n\tUtilization factor > 1")
 	return False
 
 def estimatePriority(RealTime_task):
@@ -109,11 +117,11 @@ def estimatePriority(RealTime_task):
 		if (RealTime_task[i]["WCET"] != 0):
 			if (tempPeriod > RealTime_task[i]["Period"] or tempPeriod > tasks[i]["Period"]):
 				tempPeriod = tasks[i]["Period"]
-				P = i		
+				P = i
 	return P
 
 # Bug1: Not preempting tasks
-def Simulation(hp): 
+def Simulation(hp):
 	"""
 	The real time schedulng based on Rate Monotonic scheduling is simulated here.
 	"""
@@ -122,24 +130,23 @@ def Simulation(hp):
 	RealTime_task = copy.deepcopy(tasks)
 	# validation of schedulablity neessary condition
 	for i in RealTime_task.keys():
-		RealTime_task[i]["DCT"] = RealTime_task[i]["WCET"]  
-		if (RealTime_task[i]["WCET"] > RealTime_task[i]["Period"]):				
-			print(" \n\t The task can not be completed in the specified time ! ", i )	
-	
+		RealTime_task[i]["DCT"] = RealTime_task[i]["WCET"]
+		if (RealTime_task[i]["WCET"] > RealTime_task[i]["Period"]):
+			print(" \n\t The task can not be completed in the specified time ! ", i )
+
 	# main loop for simulator
 	for t in range(hp):
 
 		# Determine the priority of the given tasks
 		priority = estimatePriority(RealTime_task)
-		global dList
 
 		if (priority != -1):    #processor is not idle
 			print("\nt{}-->t{} :TASK{}".format(t,t+1,priority))
 			# Update WCET after each clock cycle
 			RealTime_task[priority]["WCET"] -= 1
 			# For the calculation of the metrics
-			d = dict(Task=priority, Start=t, Finish=t+1)      	
-			dList.append(d.copy()) 
+			dList["TASK_%d"%priority]["start"].append(t)
+			dList["TASK_%d"%priority]["finish"].append(t+1)
 			# For plotting the results
 			y_axis.append("TASK%d"%priority)
 			from_x.append(t)
@@ -148,8 +155,8 @@ def Simulation(hp):
 		else:    #processor is idle
 			print("\nt{}-->t{} :IDLE".format(t,t+1))
 			# For the calculation of the metrics
-			d = dict(Task="-1", Start=t, Finish=t+1)
-			dList.append(d.copy())
+			dList["TASK_IDLE"]["start"].append(t)
+			dList["TASK_IDLE"]["finish"].append(t+1)
 			# For plotting the results
 			y_axis.append("IDLE")
 			from_x.append(t)
@@ -161,14 +168,14 @@ def Simulation(hp):
 			RealTime_task[i]["Period"] -= 1
 			if (RealTime_task[i]["Period"] == 0):
 				RealTime_task[i] = copy.deepcopy(tasks[i])
-		
+
 		with open('RM_sched.json','w') as outfile2:
 			json.dump(dList,outfile2,indent = 4)
 
 
 def drawGantt():
 	"""
-	The scheduled results are displayed in the form of a 
+	The scheduled results are displayed in the form of a
 	gantt chart for the user to get better understanding
 	"""
 	colors = ['red','green','blue','orange','yellow']
@@ -187,7 +194,7 @@ def drawGantt():
 def showMetrics():
 	"""
 	Displays the resultant metrics after scheduling such as
-	average response time, the average waiting time and the 
+	average response time, the average waiting time and the
 	time of first deadline miss
 	"""
 	# No need to find the start and finish time of idle task instead the total duration of idle task is required.
@@ -197,77 +204,72 @@ def showMetrics():
 	finishTime = []
 	# Calculation of number of releases
 	for i in tasks.keys():
-		N.append(int(hp)/int(tasks[i]["Period"]))
-		print ("\n Number of releases of task %d ="%i,N[i])
+		release =int(hp)/int(tasks[i]["Period"])
+		N.append(release)
+		print ("\n Number of releases of task %d ="%i,release)
 
 	# Calculation of relaese time of each task
 	for i in tasks.keys():
 		temp = []
 		for j in range(int(N[i])):
-			temp.append(j*int(tasks[i]["Period"])) 
+			temp.append(j*int(tasks[i]["Period"]))
 		# temp.append(hp)
 		releaseTime.append(temp)
 		print("\n Release time of task%d = "%i,releaseTime[i])
+	print(tasks)
 
 	# Calculation of start time of each task
-	TempstartTime = []
-	for i in tasks.keys():
-		temp = []
-		for j in range(len(dList)):
-			if dList[j]["Task"] == i:
-				temp.append(dList[j]["Start"])
-		TempstartTime.append(temp)
-
-	# Calculation of finish time of each task
-	TempfinishTime = []
-	for i in tasks.keys():
-		temp = []
-		for j in range(len(dList)):
-			if dList[j]["Task"] == i:
-				temp.append(dList[j]["Finish"])
-		TempfinishTime.append(temp)		
-
-	# oombiya part
-	# T = []
+	# TempstartTime = []
+	for j,i in enumerate(tasks.keys()):
+		start_array,end_array = filter_out(dList["TASK_%d"%i]["start"],dList["TASK_%d"%i]["finish"],N[j])
+		startTime.append(start_array)
+		finishTime.append(end_array)
+	#
+	# # Calculation of finish time of each task
+	# TempfinishTime = []
 	# for i in tasks.keys():
-
 	# 	temp = []
-	# 	for j in range(len(releaseTime[i])):
-
-	# 		t = []
-	# 		for k in range(len(TempstartTime[i])):
-	# 			if (TempstartTime[i][k] <= releaseTime[i][j]):
-	# 				t.append(TempstartTime[i][k])
-	# 		temp.append(t)
-	# 	print("temp",temp)
-
-		# for ii in range(len(temp[i])-1):
-		# 	diff =[]
-		# 	res =[]
-		# 	flag = []
-		# 	if temp[i][ii] == 0:
-		# 		startTime.append(0)
-
-		# 	elif not temp[i][ii]  and N[i] == 1:
-		# 		res = min(temp[i][ii+1])
-
-			# elif N[i] > 1:
-			# 	diff = dif(temp[i][ii+1],temp[i][ii])
-			# 	res = min(diff)
-			# startTime.append(res)	
-			# print(temp[i][ii+1],temp[i][ii])		
-		# print(releaseTime[i][j],t)
-
-				
-		# startTime.append(temp)
-		# print("\n start time of task %d = "%i,startTime[i])
-
-
+	# 	for j in range(len(dList)):
+	# 		if dList[j]["Task"] == i:
+	# 			temp.append(dList[j]["Finish"])
+	# 	TempfinishTime.append(temp)
+	#
+	#
 	for i in tasks.keys():
-		print("\n start time of task %d = "%i,TempstartTime[i])
-		print("\n finsh time of task %d = "%i,TempfinishTime[i])
+		print("\n start time of task %d = "%i,startTime[i])
+		print("\n finsh time of task %d = "%i,finishTime[i])
+		print("\n Response time of task %d = "%i,[a_i - b_i for a_i, b_i in zip(finishTime[i],releaseTime[i])])
+		print("\n Waiting time of task %d = "%i,[a_i - b_i for a_i, b_i in zip(startTime[i],releaseTime[i])])
+		print("\n")
 
-	
+def filter_out(start_array,finish_array,release_time):
+
+	print(start_array,finish_array)
+	new_start = []
+	new_finish = []
+	beg_time = min(start_array)
+	diff = int(hp/release_time)
+	if(release_time>1):
+		new_start.append(beg_time)
+		prev = beg_time
+		for i in range(int(release_time-1)):
+			beg_time = beg_time + diff
+			new_start.append(beg_time)
+			count = start_array.index(prev)
+			for i in range(start_array.index(prev),start_array.index(beg_time)-1):
+					count+=1
+			new_finish.append(finish_array[count])
+			prev = beg_time
+		new_finish.append(max(finish_array))
+
+	else:
+		end_time = max(finish_array)
+		new_start.append(beg_time)
+		new_finish.append(int(end_time))
+	return new_start,new_finish
+
+
+
 
 
 if __name__ == '__main__':
@@ -280,7 +282,7 @@ if __name__ == '__main__':
 		Simulation(hp)
 		showMetrics()
 		drawGantt()
-		
+
 	else:
 		Read_data()
 		sched_res = Schedulablity()
